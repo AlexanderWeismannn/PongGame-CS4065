6bb0ec360c68e6e630ac4884e8fb58a722b74d7f
Alex <50328533+AlexanderWeismannn@users.noreply.github.com>
2021-03-02 23:14:34 -0400

    commented out and changed a few things
    player 2 is now an AI so we dont need to look for keypresses anymore

--- player_functions.py
+++ player_functions.py
@@ -18,21 +18,24 @@ def player_animation(player1, player2, SCREEN_SIZE):
 
     return player_positions
 
-def user_input(p1_speed, p2_speed):
+def user_input(p1_speed):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+
+          
+
         #if the key has been pressed down increase the player speed
         #depending on direction
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                #move down
-                p2_speed += 7
-            if event.key == pygame.K_UP:
-                #move up
-                p2_speed -= 7
+            # if event.key == pygame.K_DOWN:
+            #     #move down
+            #     p2_speed += 7
+            # if event.key == pygame.K_UP:
+            #     #move up
+            #     p2_speed -= 7
             if event.key == pygame.K_s:
                 #move down
                 p1_speed += 7
@@ -43,16 +46,16 @@ def user_input(p1_speed, p2_speed):
         #if the key has been released revert the speed back by the amount
         #it was increased by
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_DOWN:
-                #move down
-                p2_speed -= 7
-            if event.key == pygame.K_UP:
-                #move up
-                p2_speed += 7
+            # if event.key == pygame.K_DOWN:
+            #     #move down
+            #     p2_speed -= 7
+            # if event.key == pygame.K_UP:
+            #     #move up
+            #     p2_speed += 7
             if event.key == pygame.K_s:
                 #move down
                 p1_speed -= 7
             if event.key == pygame.K_w:
                 #move up
                 p1_speed += 7
-    return p1_speed, p2_speed
+    return p1_speed
